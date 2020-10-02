ce2f607f18bcbdfdf739e21a8752a4983cebce70
Gagandeep <gaganomj.singh@mail.utoronto.ca>
2020-10-02 19:31:52 -0400

    simplified code to simply print array instead of returning

--- lotto.py
+++ lotto.py
@@ -2,9 +2,8 @@ import random
 
 def lotto():
     arr = random.sample(range(1, 50), 7)
-    return arr
+    print(arr)
 
 
 if __name__ == "__main__":
-    numbers = lotto()
-    print(numbers)
+    lotto()
